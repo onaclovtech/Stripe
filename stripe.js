2c1d340cea64d9302e38f07d7d85f6aa516f2c07
Tyson Bailey <onaclov2000+github@gmail.com>
2014-07-01 10:27:46 -0600

    Update stripe.js

--- stripe.js
+++ stripe.js
@@ -1,1 +1,18 @@
-asfd
+// Stripe
+// This really doesn't do much and shouldn't be guarenteed, but this is the general idea, basically
+// in angularjs you would be able to do a $payment.$charge to charge a customer in theory.
+// This guy should be returning promises of when it successfully send/fails charges.
+//      License: MIT
+
+  angular.module('payment', []).factory('$payment', ['$http', function($http) {
+      return { $charge : function (api_key)
+                  {
+                          var method = 'GET';
+                          var url = "https://api.stripe.com/v1/charges";
+                          $http({method: method, url: url  + "-u " + api_key  + ":"}).
+                          success(function(data, status) {}).
+                          error(function(data, status) {});   
+                  }
+            };
+    }
+  ]);
